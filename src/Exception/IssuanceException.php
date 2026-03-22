1fd3e38f0b22e6a52cbc068c89edf2a42f07b754
Vitor Mattos <vitor@php.rio>
2026-03-21 23:47:42 -0300

    feat(exceptions): add IssuanceException for rejected issuance
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/Exception/IssuanceException.php
@@ -0,0 +1,15 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace LibreCodeCoop\NfsePHP\Exception;
+
+/**
+ * Thrown when the SEFIN gateway rejects an NFS-e issuance request.
+ */
+class IssuanceException extends GatewayException
+{
+}
