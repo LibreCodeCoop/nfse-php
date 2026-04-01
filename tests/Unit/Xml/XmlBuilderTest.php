b80f6fec17e2c954ea853484f9daee1ddbb41c38
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-01 17:42:23 -0300

    test(xml): update and extend XmlBuilder tests for new structure
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/Unit/Xml/XmlBuilderTest.php
+++ tests/Unit/Xml/XmlBuilderTest.php
@@ -72,6 +72,35 @@ class XmlBuilderTest extends TestCase
 
         self::assertStringContainsString('3303302', $xml);
         self::assertStringContainsString('<cLocEmi>3303302</cLocEmi>', $xml);
+        self::assertStringContainsString('<cLocPrestacao>3303302</cLocPrestacao>', $xml);
+    }
+
+    public function testBuildDpsStartsInfDpsWithTpAmbBeforeMunicipalityFields(): void
+    {
+        $xml = $this->builder->buildDps($this->makeDps(municipioIbge: '3303302'));
+
+        $doc = new \DOMDocument();
+        $doc->loadXML($xml);
+        $xpath = new \DOMXPath($doc);
+        $xpath->registerNamespace('n', 'http://www.sped.fazenda.gov.br/nfse');
+
+        $infDps = $xpath->query('//n:infDPS')->item(0);
+        self::assertNotNull($infDps);
+
+        $headerElements = [];
+
+        foreach ($infDps?->childNodes ?? [] as $childNode) {
+            if ($childNode instanceof \DOMElement) {
+                $headerElements[] = $childNode->localName;
+            }
+        }
+
+        self::assertSame(
+            ['tpAmb', 'dhEmi', 'verAplic', 'serie', 'nDPS', 'dCompet', 'tpEmit', 'cLocEmi'],
+            array_slice($headerElements, 0, 8),
+        );
+        self::assertSame('2', $xpath->query('//n:infDPS/n:tpAmb')->item(0)?->textContent);
+        self::assertSame(0, $xpath->query('//n:infDPS/n:cMun')->length);
     }
 
     public function testBuildDpsUsesNationalTaxCodeInCtribnac(): void
@@ -79,7 +108,10 @@ class XmlBuilderTest extends TestCase
         $dps = $this->makeDps(itemListaServico: '0107', codigoTributacaoNacional: '101011');
         $xml = $this->builder->buildDps($dps);
 
-        self::assertStringContainsString('<cServ><cTribNac>101011</cTribNac></cServ>', str_replace(["\n", '  '], '', $xml));
+        self::assertStringContainsString(
+            '<cServ><cTribNac>101011</cTribNac><cTribMun>0107</cTribMun><xDescServ>Consultoria em TI</xDescServ></cServ>',
+            str_replace(["\n", '  '], '', $xml),
+        );
     }
 
     public function testBuildDpsContainsValorServico(): void
@@ -193,6 +225,10 @@ class XmlBuilderTest extends TestCase
         $nodes = $xpath->query('//n:prest/n:regTrib/n:regEspTrib');
         self::assertSame(1, $nodes->length, '<prest><regTrib><regEspTrib> must always be present');
         self::assertSame('0', $nodes->item(0)->textContent);
+
+        $simplesNodes = $xpath->query('//n:prest/n:regTrib/n:opSimpNac');
+        self::assertSame(1, $simplesNodes->length, '<prest><regTrib><opSimpNac> must always be present');
+        self::assertSame('1', $simplesNodes->item(0)->textContent);
     }
 
     public function testBuildDpsIncludesFederalTaxationBlockWhenConfigured(): void
@@ -213,16 +249,37 @@ class XmlBuilderTest extends TestCase
         $xml = $this->builder->buildDps($dps);
 
         self::assertStringContainsString('<tribFed>', $xml);
-        self::assertStringContainsString('<sitTribPISCOFINS>1</sitTribPISCOFINS>', $xml);
-        self::assertStringContainsString('<tpRetPISCOFINSCSLL>3</tpRetPISCOFINSCSLL>', $xml);
-        self::assertStringContainsString('<vBCPISCOFINS>1000.00</vBCPISCOFINS>', $xml);
-        self::assertStringContainsString('<pAliqPIS>1.65</pAliqPIS>', $xml);
-        self::assertStringContainsString('<vPIS>16.50</vPIS>', $xml);
-        self::assertStringContainsString('<pAliqCOFINS>7.60</pAliqCOFINS>', $xml);
-        self::assertStringContainsString('<vCOFINS>76.00</vCOFINS>', $xml);
-        self::assertStringContainsString('<vIRRF>15.00</vIRRF>', $xml);
-        self::assertStringContainsString('<vCSLL>10.00</vCSLL>', $xml);
-        self::assertStringContainsString('<vCP>5.00</vCP>', $xml);
+        self::assertStringContainsString('<piscofins>', $xml);
+        self::assertStringContainsString('<CST>01</CST>', $xml);
+        self::assertStringContainsString('<tpRetPisCofins>3</tpRetPisCofins>', $xml);
+        self::assertStringContainsString('<vBCPisCofins>1000.00</vBCPisCofins>', $xml);
+        self::assertStringContainsString('<pAliqPis>1.65</pAliqPis>', $xml);
+        self::assertStringContainsString('<vPis>16.50</vPis>', $xml);
+        self::assertStringContainsString('<pAliqCofins>7.60</pAliqCofins>', $xml);
+        self::assertStringContainsString('<vCofins>76.00</vCofins>', $xml);
+        self::assertStringContainsString('<vRetIRRF>15.00</vRetIRRF>', $xml);
+        self::assertStringContainsString('<vRetCSLL>10.00</vRetCSLL>', $xml);
+        self::assertStringContainsString('<vRetCP>5.00</vRetCP>', $xml);
+    }
+
+    public function testBuildDpsOmitsZeroValuedOptionalFederalRetentions(): void
+    {
+        $xml = $this->builder->buildDps($this->makeDps(
+            federalPiscofinsSituacaoTributaria: '1',
+            federalPiscofinsTipoRetencao: '4',
+            federalPiscofinsBaseCalculo: '14227.50',
+            federalPiscofinsAliquotaPis: '0.65',
+            federalPiscofinsValorPis: '92.48',
+            federalPiscofinsAliquotaCofins: '3.00',
+            federalPiscofinsValorCofins: '426.83',
+            federalValorIrrf: '472.50',
+            federalValorCsll: '0.00',
+            federalValorCp: '0.00',
+        ));
+
+        self::assertStringContainsString('<vRetIRRF>472.50</vRetIRRF>', $xml);
+        self::assertStringNotContainsString('<vRetCSLL>', $xml);
+        self::assertStringNotContainsString('<vRetCP>', $xml);
     }
 
     // -------------------------------------------------------------------------
@@ -240,15 +297,10 @@ class XmlBuilderTest extends TestCase
         );
         $xml = $this->builder->buildDps($dpsNaoOptante);
 
-        // For "não optante" (opSimpNac = 1), pAliq must NOT be present
-        // but indTotTrib is now ALWAYS included (even if 0) to avoid schema validation errors
+        // For "não optante" (opSimpNac = 1), pAliq must NOT be present.
         self::assertStringNotContainsString('<pAliq>', $xml);
-        self::assertStringContainsString('<indTotTrib>', $xml);
+        self::assertStringNotContainsString('<pTotTrib>', $xml);
 
-        // totTrib container must exist with content
-        self::assertStringContainsString('<totTrib', $xml);
-
-        // tribMun and tribISSQN must still exist
         self::assertStringContainsString('<tribMun>', $xml);
         self::assertStringContainsString('<tribISSQN>', $xml);
     }
@@ -263,13 +315,24 @@ class XmlBuilderTest extends TestCase
             valorServico: '1000.00',
             aliquota: '5.00',
             opcaoSimplesNacional: 2, // optante
-            indicadorTributacao: 1,
         );
         $xml = $this->builder->buildDps($dpsOptante);
 
-        // For "optante" (opSimpNac = 2), indTotTrib and pAliq MUST be present
-        self::assertStringContainsString('<indTotTrib>', $xml);
+        // For "optante" (opSimpNac = 2), pAliq MUST be present.
         self::assertStringContainsString('<pAliq>', $xml);
+        self::assertStringNotContainsString('<pTotTrib>', $xml);
+    }
+
+    public function testBuildDpsIncludesTotalTributosPercentuaisWhenConfigured(): void
+    {
+        $xml = $this->builder->buildDps($this->makeDps(
+            indicadorTributacao: 2,
+            totalTributosPercentualFederal: '3.65',
+            totalTributosPercentualEstadual: '0.00',
+            totalTributosPercentualMunicipal: '2.00',
+        ));
+
+        self::assertStringContainsString('<pTotTrib><pTotTribFed>3.65</pTotTribFed><pTotTribEst>0.00</pTotTribEst><pTotTribMun>2.00</pTotTribMun></pTotTrib>', str_replace(["\n", '  '], '', $xml));
     }
 
     // -------------------------------------------------------------------------
@@ -291,6 +354,9 @@ class XmlBuilderTest extends TestCase
         int $tipoRetencaoIss = 1,
         int $opcaoSimplesNacional = 1,
         int $indicadorTributacao = 0,
+        string $totalTributosPercentualFederal = '',
+        string $totalTributosPercentualEstadual = '',
+        string $totalTributosPercentualMunicipal = '',
         string $federalPiscofinsSituacaoTributaria = '',
         string $federalPiscofinsTipoRetencao = '',
         string $federalPiscofinsBaseCalculo = '',
@@ -319,6 +385,9 @@ class XmlBuilderTest extends TestCase
             issRetido:                $issRetido,
             opcaoSimplesNacional:     $opcaoSimplesNacional,
             indicadorTributacao:      $indicadorTributacao,
+            totalTributosPercentualFederal: $totalTributosPercentualFederal,
+            totalTributosPercentualEstadual: $totalTributosPercentualEstadual,
+            totalTributosPercentualMunicipal: $totalTributosPercentualMunicipal,
             federalPiscofinsSituacaoTributaria: $federalPiscofinsSituacaoTributaria,
             federalPiscofinsTipoRetencao: $federalPiscofinsTipoRetencao,
             federalPiscofinsBaseCalculo: $federalPiscofinsBaseCalculo,
