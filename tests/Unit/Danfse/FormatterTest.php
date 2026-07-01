408f4cc952811b8ec7181faae9622211fd0f5052
YvesCesar <yvesamorim73@gmail.com>
2026-07-01 15:03:01 -0400

    test(danfse): rename remaining Formatter test property usages
    Signed-off-by: YvesCesar <yvesamorim73@gmail.com>

--- tests/Unit/Danfse/FormatterTest.php
+++ tests/Unit/Danfse/FormatterTest.php
@@ -19,65 +19,65 @@ class FormatterTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->fmt = new Formatter();
+        $this->formatter = new Formatter();
     }
 
     public function testCnpjIsFormatted(): void
     {
-        self::assertSame('11.222.333/0001-81', $this->fmt->cnpjCpf('11222333000181'));
+        self::assertSame('11.222.333/0001-81', $this->formatter->cnpjCpf('11222333000181'));
     }
 
     public function testCpfIsFormatted(): void
     {
-        self::assertSame('123.456.789-09', $this->fmt->cnpjCpf('12345678909'));
+        self::assertSame('123.456.789-09', $this->formatter->cnpjCpf('12345678909'));
     }
 
     public function testCnpjCpfReturnsDashForEmpty(): void
     {
-        self::assertSame('-', $this->fmt->cnpjCpf(''));
-        self::assertSame('-', $this->fmt->cnpjCpf('-'));
+        self::assertSame('-', $this->formatter->cnpjCpf(''));
+        self::assertSame('-', $this->formatter->cnpjCpf('-'));
     }
 
     public function testCnpjCpfReturnsRawWhenLengthUnexpected(): void
     {
-        self::assertSame('12345', $this->fmt->cnpjCpf('12345'));
+        self::assertSame('12345', $this->formatter->cnpjCpf('12345'));
     }
 
     public function testPhoneWithEightDigitsLocalAndMobile(): void
     {
-        self::assertSame('(21) 3000-1234', $this->fmt->phone('2130001234'));
-        self::assertSame('(11) 98765-4321', $this->fmt->phone('11987654321'));
+        self::assertSame('(21) 3000-1234', $this->formatter->phone('2130001234'));
+        self::assertSame('(11) 98765-4321', $this->formatter->phone('11987654321'));
     }
 
     public function testCepIsFormatted(): void
     {
-        self::assertSame('24020-005', $this->fmt->cep('24020005'));
-        self::assertSame('-', $this->fmt->cep(''));
+        self::assertSame('24020-005', $this->formatter->cep('24020005'));
+        self::assertSame('-', $this->formatter->cep(''));
     }
 
     public function testDateAndDateTime(): void
     {
-        self::assertSame('15/01/2026', $this->fmt->date('2026-01-15'));
-        self::assertSame('15/01/2026 14:30:00', $this->fmt->dateTime('2026-01-15T14:30:00-03:00'));
-        self::assertSame('-', $this->fmt->date(''));
+        self::assertSame('15/01/2026', $this->formatter->date('2026-01-15'));
+        self::assertSame('15/01/2026 14:30:00', $this->formatter->dateTime('2026-01-15T14:30:00-03:00'));
+        self::assertSame('-', $this->formatter->date(''));
     }
 
     public function testCurrency(): void
     {
-        self::assertSame('R$ 1.500,00', $this->fmt->currency('1500.00'));
-        self::assertSame('R$ 1.292,75', $this->fmt->currency('1292.75'));
-        self::assertSame('-', $this->fmt->currency(''));
+        self::assertSame('R$ 1.500,00', $this->formatter->currency('1500.00'));
+        self::assertSame('R$ 1.292,75', $this->formatter->currency('1292.75'));
+        self::assertSame('-', $this->formatter->currency(''));
     }
 
     public function testCodTribNacional(): void
     {
-        self::assertSame('01.07.00', $this->fmt->codTribNacional('010700'));
-        self::assertSame('-', $this->fmt->codTribNacional(''));
+        self::assertSame('01.07.00', $this->formatter->codTribNacional('010700'));
+        self::assertSame('-', $this->formatter->codTribNacional(''));
     }
 
     public function testLimitTruncatesWithEllipsis(): void
     {
-        self::assertSame('abc', $this->fmt->limit('abc', 5));
-        self::assertSame('abcde...', $this->fmt->limit('abcdefgh', 5));
+        self::assertSame('abc', $this->formatter->limit('abc', 5));
+        self::assertSame('abcde...', $this->formatter->limit('abcdefgh', 5));
     }
 }
